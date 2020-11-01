0fd1897dbb748444edd766fdece07ae80e6799a8
Myles <mylesjanik@gmail.com>
2020-11-01 12:26:53 -0800

    commit tree 283b21ceb841833fbe3954b11d68ed0ddcf9eac3

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+    new_string = ""
+    if array.length == 1
+        new_string = array[0]
+        return new_string
+    elsif array.length == 2
+        new_string += array[0] + " and " + array[1]
+        return new_string
+    elsif array.length == 3
+        new_string += array[0] + ", " + array[1] + ", and " + array[2]
+        return new_string
+    else
+        counter = 1
+        while counter < array.length
+            new_string += array[counter - 1] + ", "
+            counter += 1
+        end
+        new_string += "and " + array[array.length - 1]
+    end
 end
